2167311a84a6e725ff78c61da9a2c7a6c8880ec4
Michael T. Becker <programming@wickedmic.com>
2017-12-22 21:53:43 +0100

    removed copy/move assign & other bugfixes
    removed copy/move assign since both assign operators
    are required to destroy the current object in case
    the other object is from another type. So assignment
    will now always replace the current object.

--- include/any/any.hpp
+++ include/any/any.hpp
@@ -11,21 +11,26 @@ namespace any
 		{
 			destroy,
 			copy,
-			copy_assign,
-			move,
-			move_assign
+			move
 		};
 
 		template<typename T> void interface(mode, void*, void*);
 
-		using interface_t = void (*)(imode, void*, void*);
+		using interface_t = void (*)(mode, void*, void*);
 
 		template<typename T>
 		struct model
 		{
+			template<typename... Args>
 			model(Args&&... args)
-				: interface(&detail::interface<T>), object(std::forward<Args>(args)...)
+				: interface(&detail::interface<T>),
+				object(std::forward<Args>(args)...)
 			{ }
+
+			model(model const&) = default;
+			model& operator= (model const&) = default;
+			model(model&&) = default;
+			model& operator= (model&&) = default;
 
 			interface_t interface;
 			T object;
@@ -36,27 +41,22 @@ namespace any
 		{
 			switch(m)
 			{
-			case mode.destory:
+			case mode::destroy:
 				reinterpret_cast<model<T>*>(ptr1)->~model<T>();
+				reinterpret_cast<model<T>*>(ptr1)->interface = nullptr;
 				break;
-			case mode.copy:
-				new(ptr1) model<T>(*reinterpret_cast<model<T>*>(ptr2));
+			case mode::copy:
+				new(ptr1) model<T>(*reinterpret_cast<model<T> const*>(ptr2));
 				break;
-			case mode.copy:
-				*reinterpret_cast<model<T>*>(ptr2) = *reinterpret_cast<model<T>*>(ptr2);
-				break;
-			case mode.move:
+			case mode::move:
 				new(ptr1) model<T>(std::move(*reinterpret_cast<model<T>*>(ptr2)));
-				break;
-			case mode.move_assign:
-				*reinterpret_cast<model<T>*>(ptr2) = *reinterpret_cast<model<T>*>(ptr2);
 				break;
 			}
 		}
 
-		std::uintptr_t typeid_by_data(void* any_data) const
+		std::uintptr_t typeid_by_data(void* any_data)
 		{
-			return reinterpret_cast<std::uintptr_t>(reinterpret_cast<detail::interface_t*>(any_data));
+			return *reinterpret_cast<std::uintptr_t*>(any_data);
 		}
 
 		template<typename T>
@@ -74,55 +74,77 @@ namespace any
 		constexpr static std::size_t size = Size;
 		constexpr static std::size_t alignment = Alignment;
 
+		template<typename _T, std::size_t _Size, std::size_t _Alignment>
+		friend _T& any_cast(any<_Size, _Alignment>& a);
+
+		template<typename _T, std::size_t _Size, std::size_t _Alignment>
+		friend bool valid_cast(any<_Size, _Alignment>& a);
+
 		~any()
 		{
 			destroy();
 		}
 
+		any()
+		{
+			std::memset(data, 0, size);
+		}
+
 		template<typename T,
 			typename = std::enable_if_t<
+				sizeof(detail::model<std::decay_t<T>>) <= size &&
+				alignof(detail::model<std::decay_t<T>>) <= alignment && 
 				std::is_copy_constructible<std::remove_reference_t<std::remove_cv_t<T>>>::value &&
-				std::is_move_constructible<std::remove_reference_t<std::remove_cv_t<T>>>::value &&
-				std::is_copy_assignable<std::remove_reference_t<std::remove_cv_t<T>>>::value &&
-				std::is_move_assignable<std::remove_reference_t<std::remove_cv_t<T>>>::value
+				std::is_move_constructible<std::remove_reference_t<std::remove_cv_t<T>>>::value
 			>
 		>
 		any(T&& object)
 		{
-			new(data) model<std::decay_t<T>>(std::forward<T>(object))
+			new(data) detail::model<std::decay_t<T>>(std::forward<T>(object));
 		}
 
 		any(any const& other)
 		{
-			interface()(detail::mode::copy, data, other.data);
+			assert(this != &other && "ill formed initialization");
+			interface(other.data)(detail::mode::copy, data, reinterpret_cast<void*>(const_cast<char*>(other.data)));
 		}
 
 		any(any&& other)
 		{
-			interface()(detail::mode::move, data, other.data);
+			assert(this != &other && "ill formed initialization");
+			interface(other.data)(detail::mode::move, data, other.data);
 		}
 
 		any& operator= (any const& other)
 		{
-			destory();
-			interface()(detail::mode::copy_assign, data, const_cast<void*>(other.data)); // const_cast to satisfy the interface; other data will only be read
+			if(this == &other)
+				return *this;
+
+			destroy();
+			interface(other.data)(detail::mode::copy, data, reinterpret_cast<void*>(const_cast<char*>(other.data))); // const_cast to satisfy the interface; other data will only be read
+			return *this;
 		}
 
 		any& operator= (any&& other)
 		{
-			destory();
-			interface()(detail::mode::move_assign, data, other.data);
+			if(this == &other)
+				return *this;
+
+			destroy();
+			interface(other.data)(detail::mode::move, data, other.data);
+			return *this;
 		}
 
 	private:
-		interface_t* interface()
+		static detail::interface_t interface(char const* data)
 		{
-			return reinterpret_cast<detail::interface_t*>(data)->interface;
+			return reinterpret_cast<detail::interface_t>(*reinterpret_cast<std::uintptr_t const*>(data));
 		}
 
 		void destroy()
 		{
-			interface()(detail::mode::destroy, data, nullptr);
+			if(*reinterpret_cast<std::uintptr_t*>(data))
+				interface(data)(detail::mode::destroy, data, nullptr);
 		}
 
 	private:
@@ -133,11 +155,17 @@ namespace any
 		char data[size];
 	};
 
-	template<typename T>
-	T& any_cast(any& a)
+	template<typename T, std::size_t Size, std::size_t Alignment>
+	bool valid_cast(any<Size, Alignment>& a)
 	{
-		assert(typeid_by_data(a.data) == typeid_by_type<T>() && "any_cast: any does not contain given type");
-		return reinterpret_cast<model<T>*>(a.data)->object;
+		return detail::typeid_by_data(a.data) == detail::typeid_by_type<T>();
+	}
+
+	template<typename T, std::size_t Size, std::size_t Alignment>
+	T& any_cast(any<Size, Alignment>& a)
+	{
+		assert(valid_cast<T>(a) && "any_cast: any does not contain given type");
+		return reinterpret_cast<detail::model<T>*>(a.data)->object;
 	}
 } // namespace any
 
--- test/test_any.cpp
+++ test/test_any.cpp
@@ -3,6 +3,105 @@
 #include <boost/test/unit_test.hpp>
 #include "any/any.hpp"
 
-BOOST_AUTO_TEST_CASE(test_any)
+BOOST_AUTO_TEST_CASE(test_any_int)
 {
+	any::any<16, 8> a = 42;
+	BOOST_TEST(any::valid_cast<int>(a));
+	auto result = any::any_cast<int>(a);
+	BOOST_TEST(result == 42);
+}
+
+struct dummy
+{
+	static bool copy;
+	static bool copy_assign;
+	static bool move;
+	static bool move_assign;
+	static bool dtor;
+
+	dummy(){ }
+	dummy(dummy const&){ copy = true; }
+	dummy& operator= (dummy const&){ copy_assign = true; return *this;}
+	dummy(dummy&&){ move = true; }
+	dummy& operator= (dummy&&){ move_assign = true; return *this;}
+	~dummy(){ dtor = true; }
+};
+bool dummy::copy = false;
+bool dummy::copy_assign = false;
+bool dummy::move = false;
+bool dummy::move_assign = false;
+bool dummy::dtor = false;
+
+
+BOOST_AUTO_TEST_CASE(test_any_dummy_class)
+{
+	{
+		dummy d{};
+		any::any<16, 8> a1 = d;
+		dummy::copy        = false;
+		dummy::copy_assign = false;
+		dummy::move        = false;
+		dummy::move_assign = false;
+		dummy::dtor        = false;
+		any::any<16,8> a2 = a1;
+		BOOST_TEST(dummy::copy        == true);
+		BOOST_TEST(dummy::copy_assign == false);
+		BOOST_TEST(dummy::move        == false);
+		BOOST_TEST(dummy::move_assign == false);
+		BOOST_TEST(dummy::dtor        == false);
+	}
+	BOOST_TEST(dummy::dtor == true);
+
+	{
+		dummy d{};
+		any::any<16, 8> a1;
+		any::any<16, 8> a2 = d;
+		dummy::copy        = false;
+		dummy::copy_assign = false;
+		dummy::move        = false;
+		dummy::move_assign = false;
+		dummy::dtor        = false;
+		a1 = a2;
+		BOOST_TEST(dummy::copy        == true);
+		BOOST_TEST(dummy::copy_assign == false);
+		BOOST_TEST(dummy::move        == false);
+		BOOST_TEST(dummy::move_assign == false);
+		BOOST_TEST(dummy::dtor        == false);
+	}
+	BOOST_TEST(dummy::dtor == true);
+
+	{
+		any::any<16, 8> a1 = dummy{};
+		dummy::copy        = false;
+		dummy::copy_assign = false;
+		dummy::move        = false;
+		dummy::move_assign = false;
+		dummy::dtor        = false;
+		any::any<16, 8> a2 = std::move(a1);
+		BOOST_TEST(dummy::copy        == false);
+		BOOST_TEST(dummy::copy_assign == false);
+		BOOST_TEST(dummy::move        == true);
+		BOOST_TEST(dummy::move_assign == false);
+		BOOST_TEST(dummy::dtor        == false);
+	}
+	BOOST_TEST(dummy::dtor == true);
+
+	{
+		dummy d{};
+		any::any<16, 8> a1;
+		any::any<16, 8> a2 = d;
+		dummy::copy        = false;
+		dummy::copy_assign = false;
+		dummy::move        = false;
+		dummy::move_assign = false;
+		dummy::dtor        = false;
+		a1 = std::move(a2);
+		BOOST_TEST(dummy::copy        == false);
+		BOOST_TEST(dummy::copy_assign == false);
+		BOOST_TEST(dummy::move        == true);
+		BOOST_TEST(dummy::move_assign == false);
+		BOOST_TEST(dummy::dtor        == false);
+	}
+	BOOST_TEST(dummy::dtor == true);
+
 }
