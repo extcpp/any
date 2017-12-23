67c2dcff8f4cfa5edf5b24ed5ef6f46b81fe5e1a
Michael T. Becker <programming@wickedmic.com>
2017-12-23 13:25:00 +0100

    added custom interfaces feature (implementation mostly redone)

--- include/any/any.hpp
+++ include/any/any.hpp
@@ -5,26 +5,78 @@
 
 namespace any
 {
-	namespace detail
+	namespace iface
 	{
-		enum class mode
+		struct placeholder
+		{ };
+
+		using _1 = placeholder;
+
+		struct copy
 		{
-			destroy,
-			copy,
-			move
+			using signature_t = void(placeholder const&, char*);
 		};
 
-		template<typename T> void interface(mode, void*, void*);
+		struct move
+		{
+			using signature_t = void(placeholder&, char*);
+		};
+	}
 
-		using interface_t = void (*)(mode, void*, void*);
+	namespace detail
+	{
+		template<typename Interface>
+		struct dispatch;
 
-		template<typename T>
+		template<typename Interface>
+		struct table_entry
+		{
+			typename dispatch<Interface>::function_t function;
+		};
+
+		/// function table for custom interfaces
+		template<typename... Interfaces>
+		struct extra_fn_table: table_entry<Interfaces>...
+		{
+			extra_fn_table(typename dispatch<Interfaces>::function_t... fn_ptrs)
+				: table_entry<Interfaces>{fn_ptrs}...
+			{ }
+		};
+
+		/// fuction table
+		template<typename... Interfaces>
+		struct fn_table
+		{
+			using destroy_fn = void (*)(char*);
+
+			fn_table(destroy_fn destroy_fn_ptr, typename dispatch<Interfaces>::function_t... fn_ptrs)
+				: destroy(destroy_fn_ptr), interfaces(fn_ptrs...)
+			{ }
+
+			/// destroy function
+			/**
+				\note destroy is explicitly implemented as first member to be able
+				      to do type comparison based on the adress stored here
+			*/
+			destroy_fn destroy;
+
+			/// custom interfaces
+			extra_fn_table<Interfaces...> interfaces;
+		};
+
+		template<typename T, typename... Interfaces>
+		void destroy(char* data);
+
+		template<typename T, typename... Interfaces>
+		fn_table<Interfaces...> const function_table(&destroy<T, Interfaces...>, dispatch<Interfaces>::template invoke<T, Interfaces...>...);
+
+		template<typename T, typename... Interfaces>
 		struct model
 		{
 			template<typename... Args>
 			model(Args&&... args)
-				: interface(&detail::interface<T>),
-				object(std::forward<Args>(args)...)
+				: table(&function_table<T, Interfaces...>)
+				, object(std::forward<Args>(args)...)
 			{ }
 
 			model(model const&) = default;
@@ -32,141 +84,220 @@ namespace any
 			model(model&&) = default;
 			model& operator= (model&&) = default;
 
-			interface_t interface;
+			fn_table<Interfaces...> const* const table;
 			T object;
 		};
 
-		template<typename T>
-		void interface(mode m, void* ptr1, void* ptr2)
+		template<typename Interface, typename Signature>
+		struct dispatch_impl;
+
+		/// interface function dispatcher (helper)
+		template<typename Interface, typename Return, typename... Params>
+		struct dispatch_impl<Interface, Return(iface::placeholder&, Params...)>
 		{
-			switch(m)
+			using function_t = Return(*)(char*, Params...);
+
+			/// converts `data` into `model<T, Interfaces>` and calls the given interface function with its `object` member
+			template<typename T, typename... Interfaces>
+			static Return invoke(char* data, Params... params)
 			{
-			case mode::destroy:
-				reinterpret_cast<model<T>*>(ptr1)->~model<T>();
-				reinterpret_cast<model<T>*>(ptr1)->interface = nullptr;
-				break;
-			case mode::copy:
-				new(ptr1) model<T>(*reinterpret_cast<model<T> const*>(ptr2));
-				break;
-			case mode::move:
-				new(ptr1) model<T>(std::move(*reinterpret_cast<model<T>*>(ptr2)));
-				break;
+				return Interface::template invoke<T>(reinterpret_cast<model<T, Interfaces...>*>(data)->object, std::forward<Params>(params)...);
 			}
-		}
+		};
 
-		std::uintptr_t typeid_by_data(void* any_data)
+		/// interface function dispatcher (helper)
+		template<typename Interface, typename Return, typename... Params>
+		struct dispatch_impl<Interface, Return(iface::placeholder const&, Params...)>
 		{
-			return *reinterpret_cast<std::uintptr_t*>(any_data);
+			using function_t = Return(*)(char const*, Params...);
+
+			/// converts `data` into `model<T,Interfaces> const*` and calls the given interface function with its `object` member
+			template<typename T, typename... Interfaces>
+			static Return invoke(char const* data, Params... params)
+			{
+				return Interface::template invoke<T>(reinterpret_cast<model<T, Interfaces...> const*>(data)->object, std::forward<Params>(params)...);
+			}
+		};
+
+		/// interface function dispatcher for `iface::copy`
+		template<>
+		struct dispatch_impl<iface::copy, void(iface::placeholder const&, char*)>
+		{
+			using function_t = void(*)(char const*, char*);
+
+			template<typename T, typename... Interfaces>
+			static void invoke(char const* data, char* target)
+			{
+				new(target) detail::model<T, Interfaces...>(reinterpret_cast<model<T, Interfaces...> const*>(data)->object);
+			}
+		};
+
+		/// interface function dispatcher for `iface::move`
+		template<>
+		struct dispatch_impl<iface::move, void(iface::placeholder&, char*)>
+		{
+			using function_t = void(*)(char*, char*);
+
+			template<typename T, typename... Interfaces>
+			static void invoke(char* data, char* target)
+			{
+				new(target) detail::model<T, Interfaces...>(std::move(reinterpret_cast<model<T, Interfaces...>*>(data)->object));
+			}
+		};
+
+		/// interface function dispatcher
+		template<typename Interface>
+		struct dispatch : dispatch_impl<Interface, typename Interface::signature_t>
+		{ };
+
+		template<typename T, typename... Interfaces>
+		void destroy(char* data)
+		{
+			reinterpret_cast<model<T, Interfaces...>*>(data)->~model<T, Interfaces...>();
 		}
 
-		template<typename T>
+		std::uintptr_t typeid_by_data(char* any_data)
+		{
+			return *reinterpret_cast<uintptr_t*>(*reinterpret_cast<std::uintptr_t*>(any_data));
+		}
+
+		template<typename T, typename... Interfaces>
 		std::uintptr_t typeid_by_type()
 		{
-			return reinterpret_cast<std::uintptr_t>(&interface<T>);
+			return reinterpret_cast<std::uintptr_t>(&destroy<T, Interfaces...>);
 		}
 
 	} // namespace detail
 
-	template<std::size_t Size, std::size_t Alignment>
-	class alignas(Alignment) any
+	template<std::size_t Size, std::size_t Alignment, typename... Interfaces>
+	class alignas(Alignment) base_any
 	{
 	public:
 		constexpr static std::size_t size = Size;
 		constexpr static std::size_t alignment = Alignment;
 
-		template<typename _T, std::size_t _Size, std::size_t _Alignment>
-		friend _T& any_cast(any<_Size, _Alignment>& a);
+		template<typename _T, std::size_t _Size, std::size_t _Alignment, typename... _Interfaces>
+		friend _T& any_cast(base_any<_Size, _Alignment, _Interfaces...>& a);
 
-		template<typename _T, std::size_t _Size, std::size_t _Alignment>
-		friend bool valid_cast(any<_Size, _Alignment>& a);
+		template<typename _T, std::size_t _Size, std::size_t _Alignment, typename... _Interfaces>
+		friend bool valid_cast(base_any<_Size, _Alignment, _Interfaces...>& a);
 
-		~any()
+		~base_any()
 		{
 			destroy();
 		}
 
-		any()
-		{
-			std::memset(data, 0, size);
-		}
-
-		template<typename T,
-			typename = std::enable_if_t<
-				sizeof(detail::model<std::decay_t<T>>) <= size &&
-				alignof(detail::model<std::decay_t<T>>) <= alignment && 
-				std::is_copy_constructible<std::remove_reference_t<std::remove_cv_t<T>>>::value &&
-				std::is_move_constructible<std::remove_reference_t<std::remove_cv_t<T>>>::value
-			>
+		template<
+			typename T,
+			typename = std::enable_if_t<!std::is_same<std::remove_reference_t<std::remove_cv_t<T>>, base_any<Size, Alignment, Interfaces...>>::value>
 		>
-		any(T&& object)
+		base_any(T&& object)
 		{
-			new(data) detail::model<std::decay_t<T>>(std::forward<T>(object));
+			static_assert(sizeof(detail::model<std::decay_t<T>>) <= size, "given object does not fit into this any");
+			static_assert(alignof(detail::model<std::decay_t<T>>) <= alignment, "given object requires a greater alignment");
+			new(data) detail::model<std::decay_t<T>, Interfaces...>(std::forward<T>(object));
 		}
 
-		any(any const& other)
+		base_any(base_any const& other)
 		{
+			static_assert(std::is_base_of<detail::table_entry<iface::copy>, detail::extra_fn_table<Interfaces...>>::value,
+				"this any has no interface for copy construction");
+
 			assert(this != &other && "ill formed initialization");
-			interface(other.data)(detail::mode::copy, data, reinterpret_cast<void*>(const_cast<char*>(other.data)));
+			interface<iface::copy>(other.data).function(other.data, data);
 		}
 
-		any(any&& other)
+		base_any(base_any&& other)
 		{
+			static_assert(std::is_base_of<detail::table_entry<iface::move>, detail::extra_fn_table<Interfaces...>>::value,
+				"this any has no interface for move construction");
+
 			assert(this != &other && "ill formed initialization");
-			interface(other.data)(detail::mode::move, data, other.data);
+			interface<iface::move>(other.data).function(other.data, data);
 		}
 
-		any& operator= (any const& other)
+		base_any& operator= (base_any const& other)
 		{
+			static_assert(std::is_base_of<detail::table_entry<iface::copy>, detail::extra_fn_table<Interfaces...>>::value,
+				"this any has no interface for copy construction");
+
 			if(this == &other)
 				return *this;
 
 			destroy();
-			interface(other.data)(detail::mode::copy, data, reinterpret_cast<void*>(const_cast<char*>(other.data))); // const_cast to satisfy the interface; other data will only be read
+			interface<iface::copy>(other.data).function(other.data, data);
 			return *this;
 		}
 
-		any& operator= (any&& other)
+		base_any& operator= (base_any&& other)
 		{
+			static_assert(std::is_base_of<detail::table_entry<iface::move>, detail::extra_fn_table<Interfaces...>>::value,
+				"this any has no interface for move construction");
+
 			if(this == &other)
 				return *this;
 
 			destroy();
-			interface(other.data)(detail::mode::move, data, other.data);
+			interface<iface::move>(other.data).function(other.data, data);
 			return *this;
+		}
+
+		template<typename Interface, typename... Args>
+		auto call(Args&&... args)
+		{
+			static_assert(std::is_base_of<detail::table_entry<iface::move>, detail::extra_fn_table<Interfaces...>>::value,
+				"this any does not support given interface");
+
+			return interface<Interface>(data).function(data, std::forward<Args>(args)...);
+		}
+
+		template<typename Interface, typename... Args>
+		auto call(Args&&... args) const
+		{
+			static_assert(std::is_base_of<detail::table_entry<iface::move>, detail::extra_fn_table<Interfaces...>>::value,
+				"this any does not support given interface");
+
+			return interface<Interface>(data).function(data, std::forward<Args>(args)...);
 		}
 
 	private:
-		static detail::interface_t interface(char const* data)
+		template<typename Interface>
+		static auto interface(char const* data)
+			-> detail::table_entry<Interface> const&
 		{
-			return reinterpret_cast<detail::interface_t>(*reinterpret_cast<std::uintptr_t const*>(data));
+			return static_cast<detail::table_entry<Interface> const&>(reinterpret_cast<detail::fn_table<Interfaces...> const*>(*reinterpret_cast<std::uintptr_t const*>(data))->interfaces);
 		}
 
 		void destroy()
 		{
-			if(*reinterpret_cast<std::uintptr_t*>(data))
-				interface(data)(detail::mode::destroy, data, nullptr);
+			auto* table = reinterpret_cast<detail::fn_table<Interfaces...> const*>(*reinterpret_cast<std::uintptr_t const*>(data));
+			if(table)
+				table->destroy(data);
 		}
 
 	private:
-		template<typename T> friend T* any_cast(any&);
-		template<typename T> friend T const* any_cast(any const&);
+		template<typename T> friend T* any_cast(base_any&);
+		template<typename T> friend T const* any_cast(base_any const&);
 
 	private:
 		char data[size];
 	};
 
-	template<typename T, std::size_t Size, std::size_t Alignment>
-	bool valid_cast(any<Size, Alignment>& a)
+	template<typename T, std::size_t Size, std::size_t Alignment, typename... Interfaces>
+	bool valid_cast(base_any<Size, Alignment, Interfaces...>& a)
 	{
-		return detail::typeid_by_data(a.data) == detail::typeid_by_type<T>();
+		return detail::typeid_by_data(a.data) == detail::typeid_by_type<T, Interfaces...>();
 	}
 
-	template<typename T, std::size_t Size, std::size_t Alignment>
-	T& any_cast(any<Size, Alignment>& a)
+	template<typename T, std::size_t Size, std::size_t Alignment, typename... Interfaces>
+	T& any_cast(base_any<Size, Alignment, Interfaces...>& a)
 	{
 		assert(valid_cast<T>(a) && "any_cast: any does not contain given type");
-		return reinterpret_cast<detail::model<T>*>(a.data)->object;
+		return reinterpret_cast<detail::model<T, Interfaces...>*>(a.data)->object;
 	}
+
+	template<std::size_t Size, std::size_t Alignment = 8>
+	using any = base_any<Size, Alignment, iface::copy>;
 } // namespace any
 
 #endif
--- test/test_any.cpp
+++ test/test_any.cpp
@@ -13,95 +13,130 @@ BOOST_AUTO_TEST_CASE(test_any_int)
 
 struct dummy
 {
-	static bool copy;
-	static bool copy_assign;
-	static bool move;
-	static bool move_assign;
-	static bool dtor;
+	static unsigned copy;
+	static unsigned copy_assign;
+	static unsigned move;
+	static unsigned move_assign;
+	static unsigned dtor;
 
 	dummy(){ }
-	dummy(dummy const&){ copy = true; }
-	dummy& operator= (dummy const&){ copy_assign = true; return *this;}
-	dummy(dummy&&){ move = true; }
-	dummy& operator= (dummy&&){ move_assign = true; return *this;}
-	~dummy(){ dtor = true; }
+	dummy(dummy const&){ ++copy; }
+	dummy& operator= (dummy const&){ ++copy_assign; return *this;}
+	dummy(dummy&&){ ++move; }
+	dummy& operator= (dummy&&){ ++move_assign; return *this;}
+	~dummy(){ ++dtor; }
 };
-bool dummy::copy = false;
-bool dummy::copy_assign = false;
-bool dummy::move = false;
-bool dummy::move_assign = false;
-bool dummy::dtor = false;
+unsigned dummy::copy        = 0;
+unsigned dummy::copy_assign = 0;
+unsigned dummy::move        = 0;
+unsigned dummy::move_assign = 0;
+unsigned dummy::dtor        = 0;
 
 
-BOOST_AUTO_TEST_CASE(test_any_dummy_class)
+BOOST_AUTO_TEST_CASE(test_any_copy_move)
 {
+	using any_t = any::base_any<16, 8, any::iface::copy, any::iface::move>;
 	{
 		dummy d{};
-		any::any<16, 8> a1 = d;
-		dummy::copy        = false;
-		dummy::copy_assign = false;
-		dummy::move        = false;
-		dummy::move_assign = false;
-		dummy::dtor        = false;
-		any::any<16,8> a2 = a1;
-		BOOST_TEST(dummy::copy        == true);
-		BOOST_TEST(dummy::copy_assign == false);
-		BOOST_TEST(dummy::move        == false);
-		BOOST_TEST(dummy::move_assign == false);
-		BOOST_TEST(dummy::dtor        == false);
+		any_t a1 = d;
+		dummy::copy        = 0;
+		dummy::copy_assign = 0;
+		dummy::move        = 0;
+		dummy::move_assign = 0;
+		dummy::dtor        = 0;
+		any_t a2 = a1;
+		BOOST_TEST(dummy::copy        == 1);
+		BOOST_TEST(dummy::copy_assign == 0);
+		BOOST_TEST(dummy::move        == 0);
+		BOOST_TEST(dummy::move_assign == 0);
+		BOOST_TEST(dummy::dtor        == 0);
 	}
-	BOOST_TEST(dummy::dtor == true);
+	BOOST_TEST(dummy::dtor == 3);
 
 	{
 		dummy d{};
-		any::any<16, 8> a1;
-		any::any<16, 8> a2 = d;
-		dummy::copy        = false;
-		dummy::copy_assign = false;
-		dummy::move        = false;
-		dummy::move_assign = false;
-		dummy::dtor        = false;
+		any_t a1 = d;
+		any_t a2 = d;
+		dummy::copy        = 0;
+		dummy::copy_assign = 0;
+		dummy::move        = 0;
+		dummy::move_assign = 0;
+		dummy::dtor        = 0;
 		a1 = a2;
-		BOOST_TEST(dummy::copy        == true);
-		BOOST_TEST(dummy::copy_assign == false);
-		BOOST_TEST(dummy::move        == false);
-		BOOST_TEST(dummy::move_assign == false);
-		BOOST_TEST(dummy::dtor        == false);
+		BOOST_TEST(dummy::copy        == 1);
+		BOOST_TEST(dummy::copy_assign == 0);
+		BOOST_TEST(dummy::move        == 0);
+		BOOST_TEST(dummy::move_assign == 0);
+		BOOST_TEST(dummy::dtor        == 1);
+		dummy::dtor = 0;
 	}
-	BOOST_TEST(dummy::dtor == true);
+	BOOST_TEST(dummy::dtor == 3);
 
 	{
-		any::any<16, 8> a1 = dummy{};
-		dummy::copy        = false;
-		dummy::copy_assign = false;
-		dummy::move        = false;
-		dummy::move_assign = false;
-		dummy::dtor        = false;
-		any::any<16, 8> a2 = std::move(a1);
-		BOOST_TEST(dummy::copy        == false);
-		BOOST_TEST(dummy::copy_assign == false);
-		BOOST_TEST(dummy::move        == true);
-		BOOST_TEST(dummy::move_assign == false);
-		BOOST_TEST(dummy::dtor        == false);
+		any_t a1 = dummy{};
+		dummy::copy        = 0;
+		dummy::copy_assign = 0;
+		dummy::move        = 0;
+		dummy::move_assign = 0;
+		dummy::dtor        = 0;
+		any_t a2 = std::move(a1);
+		BOOST_TEST(dummy::copy        == 0);
+		BOOST_TEST(dummy::copy_assign == 0);
+		BOOST_TEST(dummy::move        == 1);
+		BOOST_TEST(dummy::move_assign == 0);
+		BOOST_TEST(dummy::dtor        == 0);
 	}
-	BOOST_TEST(dummy::dtor == true);
+	BOOST_TEST(dummy::dtor == 2);
 
 	{
 		dummy d{};
-		any::any<16, 8> a1;
-		any::any<16, 8> a2 = d;
-		dummy::copy        = false;
-		dummy::copy_assign = false;
-		dummy::move        = false;
-		dummy::move_assign = false;
-		dummy::dtor        = false;
+		any_t a1 = d;
+		any_t a2 = d;
+		dummy::copy        = 0;
+		dummy::copy_assign = 0;
+		dummy::move        = 0;
+		dummy::move_assign = 0;
+		dummy::dtor        = 0;
 		a1 = std::move(a2);
-		BOOST_TEST(dummy::copy        == false);
-		BOOST_TEST(dummy::copy_assign == false);
-		BOOST_TEST(dummy::move        == true);
-		BOOST_TEST(dummy::move_assign == false);
-		BOOST_TEST(dummy::dtor        == false);
+		BOOST_TEST(dummy::copy        == 0);
+		BOOST_TEST(dummy::copy_assign == 0);
+		BOOST_TEST(dummy::move        == 1);
+		BOOST_TEST(dummy::move_assign == 0);
+		BOOST_TEST(dummy::dtor        == 1);
+		dummy::dtor = 0;
 	}
-	BOOST_TEST(dummy::dtor == true);
+	BOOST_TEST(dummy::dtor == 3);
 
+}
+
+struct myinterface
+{
+	using signature_t = double(any::iface::placeholder const&, double);
+
+	template<typename T>
+	static double invoke(T const& object, double number)
+	{
+		return object + number;
+	}
+};
+
+BOOST_AUTO_TEST_CASE(test_any_custom_interface)
+{
+	using any_t = any::base_any<16, 8, any::iface::copy, any::iface::move, myinterface>;
+
+	any_t a = 42;
+	auto result = a.call<myinterface>(0.5);
+	BOOST_TEST(result == 42.5);
+	a = 41.5f;
+	result = a.call<myinterface>(0.5);
+	BOOST_TEST(result == 42.0);
+}
+
+BOOST_AUTO_TEST_CASE(test_const_any_custom_interface)
+{
+	using any_t = any::base_any<16, 8, any::iface::copy, any::iface::move, myinterface>;
+
+	const any_t a1 = 42;
+	auto result = a1.call<myinterface>(0.5);
+	BOOST_TEST(result == 42.5);
 }
